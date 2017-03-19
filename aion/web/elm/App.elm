f3382499b2b97917e37aaae641e2779ff71bc10e
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-03-19 22:19:11 +0100

    feat(elm): Create basic Elm app using beginnerProgram

--- aion/web/elm/App.elm
+++ aion/web/elm/App.elm
@@ -1,6 +1,45 @@
 module App exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
 
 main =
-  text "Hello from Elm!"
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { username: String }
+
+model: Model
+model =
+  Model ""
+
+
+-- UPDATE
+
+type Msg
+  = Username String
+
+update: Msg -> Model -> Model
+update msg model =
+  case msg of
+    Username username ->
+      { model | username = username }
+
+
+-- VIEW
+
+view: Model -> Html Msg
+view model =
+  div []
+    [ p [] [text "Welcome to Aion!"]
+    , input [ type_ "text", placeholder "Username", onInput Username] []
+    ]
