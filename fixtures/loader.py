ee13ee6a0d8615f5a1a42fbf9c79640b8b1bfac1
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-03-31 17:47:56 +0200

    feature(fixtures): Introduce question parser
    Introduce a Python module enabling one to parse files containing
    questions to Python dict objects.

---
+++ fixtures/loader.py
@@ -0,0 +1,21 @@
+import argparse
+import questionparser
+
+
+def load_questions(path='jpks'):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dump questions in postgresql database.')
+    parser.add_argument('-path', type=str, nargs='*',
+                        help="Provide path to file/folder containing questions in a proper format")
+    args = parser.parse_args()
+    paths = args.path
+    if paths is None:
+        paths = ['jpks/']
+    # import pdb
+    # pdb.set_trace()
+    for path in paths:
+        questions = questionparser.parse(path)
+    print(questions)
