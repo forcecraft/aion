9b6e0dd826b948b7e5d02bd1c5a783720737bbbf
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-04-01 08:46:50 +0200

    fix(fixtures): Include trailing whitespace in regex
    Sometimes the questions don't finish with a newline character but with
    an additional space

--- fixtures/questionparser.py
+++ fixtures/questionparser.py
@@ -3,24 +3,12 @@ import os, re
 from codingfix import fix_coding
 
 
-def explore_directory(path):
-    questions = list()
-    for elem in os.listdir(path):
-        elem_path = os.path.join(path, elem)
-        if os.path.isfile(elem_path):
-            questions.extend(parse_file(elem_path))
-
-    return questions
-
-
-# P\n%(?P<subject>([^%]|\n)*?)%\n(\$(?P<image>([^$]|\n)*?)\$\n)?@(?P<question>([^@]|\n)*?)@\n@(?P<answers>.*)@\n
-
 def parse_file(path):
     question_pattern = r'P\n' \
-                       '%(?P<subject>([^%]|\n)*?)%\n' \
+                       '%(?P<subject>.*?)%\n' \
                        '(\$(?P<image>([^\$]|\n)*?)\$\n)?' \
                        '@(?P<question>([^@]|\n)*?)@\n' \
-                       '@(?P<answers>([^@]|\n)*?)@\n'
+                       '@(?P<answers>([^@]|\n)*?)@\s*?'
 
     questions = list()
     with open(path, 'r') as f:
@@ -31,6 +19,17 @@ def parse_file(path):
             question = fix_coding(question)
             questions.append(question)
 
+    return questions
+
+
+def explore_directory(path):
+    def gather_questions():
+        for elem in os.listdir(path):
+            elem_path = os.path.join(path, elem)
+            if os.path.isfile(elem_path):
+                yield parse_file(elem_path)
+
+    questions = [question for questions in list(gather_questions()) for question in questions]
     return questions
 
 
