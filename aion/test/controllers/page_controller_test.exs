220317fed4f5c9b502f68ac2aabc9aff07de653f
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-04-05 20:13:44 +0200

    fix(test): Fix and improve page_controller_test

--- aion/test/controllers/page_controller_test.exs
+++ aion/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Aion.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert String.contains?(conn.resp_body, "Hello Aion!")
   end
 end
