527c41296c04f6d084c12813b503b10ed6436baf
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2017-04-09 16:54:37 +0200

    feature/(Addict): Add addict library for authentication.

--- aion/mix.exs
+++ aion/mix.exs
@@ -30,7 +30,9 @@ defmodule Aion.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:addict, "~> 0.3"},
+     {:mailgun, github: "chrismccord/mailgun", branch: "master", override: true},
+     {:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
