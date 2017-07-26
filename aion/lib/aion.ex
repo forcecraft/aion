8857d45f7dccce39601151538254b3553b377064
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-07-26 22:27:19 +0200

    Fix code according to linter's suggestions and set up default linter's config

--- aion/lib/aion.ex
+++ aion/lib/aion.ex
@@ -1,6 +1,10 @@
 defmodule Aion do
+  @moduledoc """
+  Main application module. Define all supervisors and workers here.
+  """
   use Application
 
+  alias Aion.{Repo, Endpoint, ChannelMonitor}
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -9,12 +13,12 @@ defmodule Aion do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(Aion.Repo, []),
+      supervisor(Repo, []),
       # Start the endpoint when the application starts
-      supervisor(Aion.Endpoint, []),
+      supervisor(Endpoint, []),
       # Start your own worker by calling: Aion.Worker.start_link(arg1, arg2, arg3)
       # worker(Aion.Worker, [arg1, arg2, arg3]),
-      worker(Aion.ChannelMonitor, [%{}])
+      worker(ChannelMonitor, [%{}])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
@@ -26,7 +30,7 @@ defmodule Aion do
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
-    Aion.Endpoint.config_change(changed, removed)
+    Endpoint.config_change(changed, removed)
     :ok
   end
 end
--- aion/web/channels/user_record.ex
+++ aion/web/channels/user_record.ex
@@ -1,3 +1,6 @@
 defmodule Aion.UserRecord do
+  @moduledoc """
+  This struct represents a player's game state in certain room.
+  """
   defstruct name: "anonymous", score: 0
 end
