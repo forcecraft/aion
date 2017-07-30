24e08689fbe777cc2935593d5bd1b41859f8d3bb
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-07-30 11:18:15 +0200

    Add basic monitor tests

---
+++ aion/test/lib/room_channel/monitor_test.exs
@@ -0,0 +1,24 @@
+defmodule Aion.MonitorTest do
+  use Aion.ModelCase
+  alias Aion.RoomChannel.Monitor
+
+  @room_id 1
+  @invalid_room_id 0
+
+  setup do
+    Monitor.create(@room_id)
+    :ok
+  end
+
+  test "create new room" do
+    assert Monitor.exists?(@room_id)
+  end
+
+  test "room does not exist" do
+    assert not Monitor.exists?(@invalid_room_id)
+  end
+
+  test "room already exists" do
+    assert Monitor.create(@room_id) == {:error, :room_already_exists}
+  end
+end
