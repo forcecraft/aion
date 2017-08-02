9e789026e36a5ba172eef745cddac14a0d942ab0
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-08-02 22:38:59 +0200

    Add on delete options to migrations

--- aion/priv/repo/migrations/20170708113722_create_room_subject.exs
+++ aion/priv/repo/migrations/20170708113722_create_room_subject.exs
@@ -3,8 +3,8 @@ defmodule Aion.Repo.Migrations.CreateRoomSubject do
 
   def change do
     create table(:room_subjects) do
-      add :room_id, references(:rooms, on_delete: :nothing)
-      add :subject_id, references(:subjects, on_delete: :nothing)
+      add :room_id, references(:rooms, on_delete: :delete_all)
+      add :subject_id, references(:subjects, on_delete: :delete_all)
 
       timestamps()
     end
