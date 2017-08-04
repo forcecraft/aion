eaefe2f75ae182f44c6ff3150ea63c4b6e2e7f51
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2017-08-04 18:24:06 +0200

    feature/(General/Utils.elm): Add colorList and defaultColor helper functions. There we can keep available and default colors for our rooms in Room Panel. Add sliceList function which is responsible for dividing given list by given number. For example: sliceList 2 [1,2,3,4] returns [[1,2],[3,4]]

--- aion/web/elm/src/General/Utils.elm
+++ aion/web/elm/src/General/Utils.elm
@@ -19,3 +19,22 @@ getSubjectIdByName result roomName =
 
         _ ->
             0
+
+colorList : Array (String)
+colorList = fromList ["#1abc9c", "#2ecc71", "#9b59b6", "#34495e", "#f1c40f", "#e74c3c", "#d35400"]
+
+defaultColor : String
+defaultColor = "#1abc9c"
+
+
+sliceList : Int -> List a -> List (List a)
+sliceList n list =
+    case (n, list) of
+        (0, list) ->
+            [list]
+
+        (n, []) ->
+            []
+
+        (n, list) ->
+            (List.take n list) :: (sliceList n (List.drop n list))
