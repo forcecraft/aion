c6ff3d944230fd763c32fd19aa104fccb57a3330
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2017-08-04 18:28:38 +0200

    feature/(General/View.elm): Replace simple listing rooms with displaying it as tiles.

--- aion/web/elm/src/General/View.elm
+++ aion/web/elm/src/General/View.elm
@@ -1,12 +1,17 @@
 module General.View exposing (..)
 
 import General.Models exposing (Model)
-import Html exposing (Html, a, div, i, li, p, text, ul)
-import Html.Attributes exposing (href)
+import General.Utils exposing (sliceList, colorList, defaultColor)
+import Html exposing (Html, a, div, i, li, p, text, ul, h2, button)
+import Html.Attributes exposing (href, style, class)
 import Msgs exposing (Msg)
 import Routing exposing (panelPath, roomsPath)
 import RemoteData exposing (WebData)
-import Room.Models exposing (RoomsData)
+import Room.Models exposing (RoomsData, Room)
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.CDN as CDN
+import Array
 
 
 notFoundView : Html Msg
@@ -30,7 +35,8 @@ homeView model =
 roomListView : Model -> Html Msg
 roomListView model =
     div []
-        [ div [] [ text "Rooms:" ]
+        [ CDN.stylesheet
+        , div [ class "room-select-title" ] [ h2 [ ] [ text "Select Room To Play:" ] ]
         , listRooms model.rooms
         ]
 
@@ -45,8 +51,49 @@ listRooms response =
             text "Loading..."
 
         RemoteData.Success roomsData ->
-            ul []
-                (List.map (\room -> li [] [ a [ href ("#rooms/" ++ (toString room.id)) ] [ text room.name ] ]) roomsData.data)
+            listAvailableRooms roomsData
 
         RemoteData.Failure error ->
             text (toString error)
+
+
+listAvailableRooms : RoomsData -> Html Msg
+listAvailableRooms roomsData =
+    let
+        sortedRooms = List.sortBy .name roomsData.data
+        slicedRooms = sliceList 6 sortedRooms
+    in
+        Grid.container []
+            (List.map listRoomsSlice slicedRooms)
+
+
+listRoomsSlice : List Room -> Html Msg
+listRoomsSlice rooms =
+    Grid.row [] (List.map listSingleRoom rooms)
+
+
+
+listSingleRoom : Room -> Grid.Column Msg
+listSingleRoom room =
+    Grid.col [ Col.lg2, Col.md4 ]
+             [
+               div [
+                     style [("backgroundColor", generateColor room)], class "tile"
+                   ]
+                   [
+                     a [ (href ("#rooms/" ++ (toString room.id))) ] [ text room.name ]
+                   ]
+             ]
+
+
+generateColor : Room -> String
+generateColor room =
+    let
+        generatedIndex = room.id * String.length(room.name) % 7
+        pickedColor = Array.get generatedIndex colorList
+    in
+        case pickedColor of
+            Just color ->
+                color
+            Nothing ->
+                defaultColor
