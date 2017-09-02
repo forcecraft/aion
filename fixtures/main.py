b1b3b22854b3f564d843a625163fe1279f66236a
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-09-02 15:37:33 +0200

    fix(script): Extract connecting to database part

--- fixtures/main.py
+++ fixtures/main.py
@@ -2,6 +2,8 @@ import argparse
 from src.loader import load_questions
 from src.questionparser import get_questions
 from src.populate_rooms import populate_one_subject_rooms, populate_all_subjects_rooms
+from src.connect_to_db import connect_to_db
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Dump questions in postgresql database.')
@@ -9,12 +11,14 @@ if __name__ == '__main__':
                         help="Provide path to file/folder containing questions in a proper format")
     args = parser.parse_args()
     paths = args.path
+    conn = connect_to_db()
+
     if paths is None:
         paths = ['jpks/']
 
     for path in paths:
         questions = get_questions(path)
 
-    load_questions(questions)
-    populate_one_subject_rooms()
-    populate_all_subjects_rooms()
+    load_questions(questions, conn)
+    populate_one_subject_rooms(conn)
+    populate_all_subjects_rooms(conn)
--- fixtures/src/populate_rooms.py
+++ fixtures/src/populate_rooms.py
@@ -1,13 +1,9 @@
-import psycopg2
-
 from src.queries.room_subjects import get_or_insert_rooms_to_subjects
 from src.queries.rooms import get_or_insert_room
 from src.queries.subjects import get_subject_dict
-from src.config import dbname, user, password, host
 
 
-def populate_one_subject_rooms():
-    conn = psycopg2.connect("dbname={} user={} password={} host={}".format(dbname, user, password, host))
+def populate_one_subject_rooms(conn):
     subject_dict = get_subject_dict(conn)
 
     for subject_id, subject_name in subject_dict.items():
@@ -15,8 +11,7 @@ def populate_one_subject_rooms():
         get_or_insert_rooms_to_subjects(room_id, subject_id, conn)
 
 
-def populate_all_subjects_rooms():
-    conn = psycopg2.connect("dbname={} user={} password={} host={}".format(dbname, user, password, host))
+def populate_all_subjects_rooms(conn):
     subjects_id_list = list(get_subject_dict(conn).keys())
     rooms_number = 5
 
