f01e51abab1552785d50e8fe37a5b460540ad3c3
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-09-06 22:24:56 +0200

    Force ssl and https

--- aion/config/prod.exs
+++ aion/config/prod.exs
@@ -2,7 +2,8 @@ use Mix.Config
 
 config :aion, Aion.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "aion-agh.herokuapps.com", port: 80],
+  url: [scheme: "https", host: "aion-agh.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   hostname: "aion-agh.herokuapps.com"
