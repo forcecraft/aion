0f3850f063b5bea3af33d95939b86ca71e81b009
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-09-06 21:09:38 +0200

    Make app compile to bundle.js

--- aion/brunch-config.js
+++ aion/brunch-config.js
@@ -36,7 +36,8 @@ exports.config = {
             executablePath: '../../node_modules/elm/binwrappers',
             elmFolder: 'web/elm',
             mainModules: ['src/App.elm'],
-            outputFolder: '../static/vendor',
+            outputFolder: '../../priv/static/js',
+            outputFile: "bundle.js",
             makeParameters: ['--warn']
         },
         babel: {
