98d1f9b019986ad9eee4e60713a28ce26f0fd31d
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-10-11 16:24:58 +0200

    Prevent errors on /api/me

--- aion/web/controllers/user_controller.ex
+++ aion/web/controllers/user_controller.ex
@@ -3,7 +3,6 @@ defmodule Aion.UserController do
 
   def get_user_info(conn, _params) do
     user = Guardian.Plug.current_resource(conn)
-    # add case user of to prevent 500
     render(conn, "user.json", user: user)
   end
 end
--- aion/web/views/user_view.ex
+++ aion/web/views/user_view.ex
@@ -1,9 +1,13 @@
 defmodule Aion.UserView do
   use Aion.Web, :view
 
-   def render("user.json", %{user: user}) do
-     %{id: user.id,
-       name: user.name,
-       email: user.email}
-   end
+  def render("user.json", %{user: user}) do
+    if user != nil do
+      %{id: user.id,
+      name: user.name,
+      email: user.email}
+    else
+      %{}
+    end
+  end
 end
