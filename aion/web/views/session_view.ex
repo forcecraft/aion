3a88577395f8f3c44d79639fdfae608861003abc
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-10-11 15:41:17 +0200

    Remove temp function

--- aion/web/views/session_view.ex
+++ aion/web/views/session_view.ex
@@ -1,10 +1,6 @@
 defmodule Aion.SessionView do
   use Aion.Web, :view
 
-  def render("data.json", %{}) do
-    %{"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"}
-  end
-
   def render("error.json", %{}) do
     %{"data": "failed to authorise"}
   end
