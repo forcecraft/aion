393a68573afb40a3b72ee9f50be2c9b32a29edb2
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-10-13 13:04:26 +0200

    Remove unused functions

--- aion/web/auth/auth.ex
+++ aion/web/auth/auth.ex
@@ -5,26 +5,11 @@ defmodule Aion.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
-  def init(opts) do
-    Keyword.fetch!(opts, :repo)
-  end
-
-  def call(conn, repo) do
-    user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(Aion.User, user_id)
-    assign(conn, :current_user, user)
-  end
-
   def login(conn, user) do
     conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
-  end
-
-  def fail(conn) do
-    conn
-    |> assign(:current_user, "failed")
   end
 
   def login_by_username_and_pass(conn, email, given_pass, opts) do
