b7c14d45fc32bbcf60e97d7a7ee7eb4dea545716
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-10-17 22:08:05 +0200

    Refactor login tests

--- aion/test/auth/auth_test.exs
+++ aion/test/auth/auth_test.exs
@@ -4,28 +4,13 @@ defmodule Aion.AuthTest do
 
   @user %User{name: "name", email: "email", password: "password", id: 5}
 
-  test "login should set current_user", %{conn: conn} do
+  test "login should set current_user, add user_id to session and set plug_session_info to :renew", %{conn: conn} do
     conn = conn
      |> Plug.Test.init_test_session(foo: "bar")
      |> Aion.Auth.login(@user)
 
     assert conn.assigns.current_user == @user
-  end
-
-  test "login should put user_id to session key", %{conn: conn} do
-    conn = conn
-     |> Plug.Test.init_test_session(foo: "bar")
-     |> Aion.Auth.login(@user)
-
-    IO.inspect conn
     assert conn.private.plug_session["user_id"] == @user.id
-  end
-
-  test "login should configure session to renew", %{conn: conn} do
-    conn = conn
-     |> Plug.Test.init_test_session(foo: "bar")
-     |> Aion.Auth.login(@user)
-
     assert conn.private.plug_session_info == :renew
   end
 end
