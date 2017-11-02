bfc186fc41e853e295a9af8fa2747c4a61b81c52
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-11-02 17:41:31 +0100

    Update chronicle test

--- aion/test/channels/question_chronicle_test.exs
+++ aion/test/channels/question_chronicle_test.exs
@@ -7,10 +7,8 @@ defmodule Aion.QuestionChronicleTest do
 
   test "question chronicle" do
     assert QuestionChronicle.list_entries == %{}
+
     QuestionChronicle.initialize_room_state(@room_id, fn -> @current_time end)
-
-    QuestionChronicle.change_room_state(@room_id, fn -> @current_time end)
-
     assert QuestionChronicle.list_entries == %{@room_id => {@current_time, :question}}
 
     time_called = @current_time + QuestionChronicle.question_timeout_micro - 1000
@@ -18,5 +16,9 @@ defmodule Aion.QuestionChronicleTest do
 
     time_called = @current_time + QuestionChronicle.question_timeout_micro
     assert QuestionChronicle.should_change?(@room_id, fn -> time_called end)
+
+    QuestionChronicle.change_room_state(@room_id, fn -> @current_time end)
+    assert QuestionChronicle.list_entries == %{@room_id => {@current_time, :break}}
+
   end
 end
