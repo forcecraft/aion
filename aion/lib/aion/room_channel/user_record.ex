853d512247fbe0a641231931256cb4a42594e3e6
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-11-03 12:36:08 +0100

    Add questions_asked field to user record

--- aion/lib/aion/room_channel/user_record.ex
+++ aion/lib/aion/room_channel/user_record.ex
@@ -2,11 +2,16 @@ defmodule Aion.RoomChannel.UserRecord do
   @moduledoc """
   This struct represents a player's game state in certain room.
   """
-  @type t :: %__MODULE__{username: String.t, score: integer}
+  @type t :: %__MODULE__{
+    username: String.t,
+    score: integer,
+    questions_asked: integer,
+  }
 
   @enforce_keys [:username]
   defstruct username: "anonymous",
-            score: 0
+    score: 0,
+    questions_asked: 0
 
   @spec update_score(__MODULE__.t, integer) :: __MODULE__.t
   def update_score(user_record, amount \\ 1) do
