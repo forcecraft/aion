813f8f047554de43d364a4a2d7664424ae12d131
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2017-11-04 18:35:25 +0100

    Create Ranking module.

---
+++ aion/lib/aion/ranking.ex
@@ -0,0 +1,19 @@
+defmodule Aion.Ranking do
+  @moduledoc """
+  This module provides logic for generating rankings
+  """
+  import Ecto.Query, only: [from: 2]
+  alias Aion.{Repo, User, UserCategoryScore}
+
+  @type user_general_score :: %{user_name: String.t, score: integer}
+
+  @spec general :: list user_general_score
+  def general do
+    Repo.all(
+      from q in User,
+      join: ucs in UserCategoryScore, on: q.id == ucs.user_id,
+      group_by: q.name,
+      select: %{user_name: q.name, score: sum(ucs.score)}
+    )
+  end
+end
---
+++ aion/test/lib/ranking_test.exs
@@ -0,0 +1,18 @@
+defmodule Aion.RankingTest do
+  use Aion.ModelCase
+  alias Aion.{Repo, User, UserCategoryScore, Ranking}
+
+  describe "Ranking.general" do
+    test "returns scored points in each category for each user" do
+      user1 = Repo.insert! User.registration_changeset(%User{}, %{name: "John", email: "test1@example.com", password: "test123"})
+      user2 = Repo.insert! User.registration_changeset(%User{}, %{name: "Michael", email: "test2@example.com", password: "test123"})
+      Repo.insert! %UserCategoryScore{user: user1, score: 2}
+      Repo.insert! %UserCategoryScore{user: user1, score: 4}
+      Repo.insert! %UserCategoryScore{user: user2, score: 5}
+
+      result = Ranking.general()
+
+      assert [%{user_name: "John", score: 6},%{user_name: "Michael", score: 5}] = result
+    end
+  end
+end
