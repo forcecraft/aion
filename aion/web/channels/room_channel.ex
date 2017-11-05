92ddef83c9dc1e87b10b1d39bb6c1d268cd0ec2d
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-11-05 12:40:17 +0100

    Introduce question breaks after successful answers

--- aion/web/channels/room_channel.ex
+++ aion/web/channels/room_channel.ex
@@ -5,12 +5,14 @@ defmodule Aion.RoomChannel do
   """
 
   use Phoenix.Channel, log_join: false, log_handle_in: :info
+
   alias Aion.{
     Presence,
     RoomChannel.Monitor,
     QuestionChronicle,
-    UserSocket,
+    UserSocket
   }
+
   require Logger
 
   @answer_feedback_topic "answer_feedback"
@@ -20,27 +22,32 @@ defmodule Aion.RoomChannel do
   @question_break_topic "question_break"
   @user_list_topic "user_list"
 
-  @spec join(String.t, %{}, UserSocket.t) :: {:ok, UserSocket.t}
+  # Upon entering question_break state, the next question will be sent after
+  # the @next_question_delay timeout
+  @next_question_delay 1000
+
+  @spec join(String.t(), %{}, UserSocket.t()) :: {:ok, UserSocket.t()}
   def join("room:" <> room_id, _params, socket) do
     username = UserSocket.get_user_name(socket)
+
     # NOTE: this is a temporary solution.
     # In the future, this function should return an error if a user wants to join a room that does not exist.
     if not Monitor.exists?(room_id) do
-        Monitor.create(room_id)
+      Monitor.create(room_id)
 
-        # NOTE: this is ineeded to start the room_state_timeout cycle
-        {:ok, timeout} = QuestionChronicle.initialize_room_state(room_id)
-        :timer.send_after(timeout, :room_state_timeout)
+      # NOTE: this is ineeded to start the room_state_timeout cycle
+      {:ok, timeout} = QuestionChronicle.initialize_room_state(room_id)
+      :timer.send_after(timeout, :room_state_timeout)
     end
 
     Monitor.user_joined(room_id, username)
     Logger.info("[channel] #{username} joined room #{room_id}")
 
     Presence.track(socket, username, %{
-          online_at: inspect(System.system_time(:seconds))
+      online_at: inspect(System.system_time(:seconds))
     })
 
-    send self(), :after_join
+    send(self(), :after_join)
     {:ok, socket}
   end
 
@@ -53,8 +60,8 @@ defmodule Aion.RoomChannel do
     Logger.info("[channel] #{username} left: " <> Kernel.inspect(msg))
 
     if Monitor.get_scores(room_id) == [] do
-        Monitor.shutdown(room_id)
-        QuestionChronicle.remove_room_state(room_id)
+      Monitor.shutdown(room_id)
+      QuestionChronicle.remove_room_state(room_id)
     end
   end
 
@@ -62,13 +69,11 @@ defmodule Aion.RoomChannel do
     username = UserSocket.get_user_name(socket)
     user_id = UserSocket.get_user_id(socket)
     evaluation = Monitor.new_answer(room_id, answer, username, user_id)
-    send_feedback socket, evaluation
+    send_feedback(socket, evaluation)
 
     if evaluation == 1.0 do
       send_scores(socket)
-      change_question(socket)
-      send_current_question(socket)
-      QuestionChronicle.change_room_state(room_id)
+      become_in_question_break_state(socket)
     end
 
     {:noreply, socket}
@@ -89,14 +94,9 @@ defmodule Aion.RoomChannel do
       Logger.info("[channel] Question timed out in room: #{room_id}. Fetching a new one...")
 
       case old_state do
-        :question -> send_question_break(socket)
-        :break -> send_display_question(socket)
+        :question -> become_in_question_break_state(socket)
+        :break -> become_in_question_displayed_state(socket)
       end
-
-      {:ok, {timeout, state}} = QuestionChronicle.change_room_state(room_id)
-      Logger.debug(fn -> "[channel] Current state is now #{state} with timeout #{timeout}" end)
-
-      :timer.send_after(timeout, :room_state_timeout)
     else
       Logger.error(fn -> "[channel] Timer went off in room: #{room_id}. Too early, though." end)
     end
@@ -109,25 +109,44 @@ defmodule Aion.RoomChannel do
     {:noreply, socket}
   end
 
-  intercept ["presence_diff"]
+  intercept(["presence_diff"])
 
   def handle_out("presence_diff", %{joins: _, leaves: _}, socket) do
-      # NOTE: This function currently sends scores.
-      # 1. We may update it later to only send the presence diff and handle it
-      # properly on the frontend side
-      # 2. The send scores here is called only when there are > 1 people in
-      # the room. If you're the first person joining the room, you're going
-      # to receive the scores from :after_join.
-      send_scores(socket)
+    # NOTE: This function currently sends scores.
+    # 1. We may update it later to only send the presence diff and handle it
+    # properly on the frontend side
+    # 2. The send scores here is called only when there are > 1 people in
+    # the room. If you're the first person joining the room, you're going
+    # to receive the scores from :after_join.
+    send_scores(socket)
 
-      {:noreply, socket}
+    {:noreply, socket}
+  end
+
+  defp become_in_question_break_state(socket) do
+    send_question_break(socket)
+    new_state_with_timer(socket)
+  end
+
+  defp become_in_question_displayed_state(socket) do
+    send_display_question(socket)
+    new_state_with_timer(socket)
+  end
+
+  defp new_state_with_timer(socket) do
+    room_id = UserSocket.get_room_id(socket)
+
+    {:ok, {timeout, state}} = QuestionChronicle.change_room_state(room_id)
+    Logger.debug(fn -> "[channel] Current state is now #{state} with timeout #{timeout}" end)
+
+    :timer.send_after(timeout, :room_state_timeout)
   end
 
   defp send_scores(socket) do
     room_id = UserSocket.get_room_id(socket)
 
     scores = Monitor.get_scores(room_id)
-    broadcast! socket, @user_list_topic, %{users: scores}
+    broadcast!(socket, @user_list_topic, %{users: scores})
   end
 
   defp change_question(socket) do
@@ -136,13 +155,13 @@ defmodule Aion.RoomChannel do
   end
 
   defp send_display_question(socket) do
-    broadcast! socket, @display_question_topic, %{}
+    broadcast!(socket, @display_question_topic, %{})
   end
 
   defp send_question_break(socket) do
-    broadcast! socket, @question_break_topic, %{}
+    broadcast!(socket, @question_break_topic, %{})
     change_question(socket)
-    :timer.send_after(1000, :next_question_timeout)
+    :timer.send_after(@next_question_delay, :next_question_timeout)
   end
 
   defp send_current_question(socket) do
@@ -151,16 +170,20 @@ defmodule Aion.RoomChannel do
     question = Monitor.get_current_question(room_id)
     image_name = if question.image_name == nil, do: "", else: question.image_name
 
-    broadcast! socket, @current_question_topic, %{content: question.content, image_name: image_name}
+    broadcast!(socket, @current_question_topic, %{
+      content: question.content,
+      image_name: image_name
+    })
   end
 
   defp send_feedback(socket, evaluation) do
-    feedback = cond do
-      evaluation == 1.0 -> :correct
-      evaluation > 0.8 -> :close
-      true -> :incorrect
-    end
+    feedback =
+      cond do
+        evaluation == 1.0 -> :correct
+        evaluation > 0.8 -> :close
+        true -> :incorrect
+      end
 
-    push socket, @answer_feedback_topic, %{"feedback" => feedback}
+    push(socket, @answer_feedback_topic, %{"feedback" => feedback})
   end
 end
