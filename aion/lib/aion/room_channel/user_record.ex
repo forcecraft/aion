2c15b0e97191374b06b4b5a079a7a525d9ba99c4
Maciej Rapacz <mrapacz@users.noreply.github.com>
2017-11-05 22:28:45 +0100

    feature: Improve score displaying (#162)

--- aion/lib/aion/room_channel/user_record.ex
+++ aion/lib/aion/room_channel/user_record.ex
@@ -2,14 +2,24 @@ defmodule Aion.RoomChannel.UserRecord do
   @moduledoc """
   This struct represents a player's game state in certain room.
   """
-  @type t :: %__MODULE__{username: String.t, score: integer}
+  @type t :: %__MODULE__{
+    username: String.t,
+    score: integer,
+    questions_asked: integer,
+  }
 
   @enforce_keys [:username]
   defstruct username: "anonymous",
-            score: 0
+    score: 0,
+    questions_asked: 0
 
   @spec update_score(__MODULE__.t, integer) :: __MODULE__.t
   def update_score(user_record, amount \\ 1) do
     Map.update!(user_record, :score, &(&1 + amount))
+  end
+
+  @spec bump_questions_asked(__MODULE__.t) :: __MODULE__.t
+  def bump_questions_asked(user_record) do
+    Map.update!(user_record, :questions_asked, &(&1 + 1))
   end
 end
--- aion/web/channels/room_channel.ex
+++ aion/web/channels/room_channel.ex
@@ -5,12 +5,14 @@ defmodule Aion.RoomChannel do
   """
 
   use Phoenix.Channel, log_join: false, log_handle_in: :info
+
   alias Aion.{
     Presence,
     RoomChannel.Monitor,
     QuestionChronicle,
-    UserSocket,
+    UserSocket
   }
+
   require Logger
 
   @answer_feedback_topic "answer_feedback"
@@ -20,27 +22,28 @@ defmodule Aion.RoomChannel do
   @question_break_topic "question_break"
   @user_list_topic "user_list"
 
-  @spec join(String.t, %{}, UserSocket.t) :: {:ok, UserSocket.t}
+  @spec join(String.t(), %{}, UserSocket.t()) :: {:ok, UserSocket.t()}
   def join("room:" <> room_id, _params, socket) do
     username = UserSocket.get_user_name(socket)
+
     # NOTE: this is a temporary solution.
     # In the future, this function should return an error if a user wants to join a room that does not exist.
     if not Monitor.exists?(room_id) do
-        Monitor.create(room_id)
+      Monitor.create(room_id)
 
-        # NOTE: this is ineeded to start the room_state_timeout cycle
-        {:ok, timeout} = QuestionChronicle.initialize_room_state(room_id)
-        :timer.send_after(timeout, :room_state_timeout)
+      # NOTE: this is ineeded to start the room_state_timeout cycle
+      {:ok, timeout} = QuestionChronicle.initialize_room_state(room_id)
+      :timer.send_after(timeout, :room_state_timeout)
     end
 
     Monitor.user_joined(room_id, username)
     Logger.info("[channel] #{username} joined room #{room_id}")
 
     Presence.track(socket, username, %{
-          online_at: inspect(System.system_time(:seconds))
+      online_at: inspect(System.system_time(:seconds))
     })
 
-    send self(), :after_join
+    send(self(), :after_join)
     {:ok, socket}
   end
 
@@ -53,8 +56,8 @@ defmodule Aion.RoomChannel do
     Logger.info("[channel] #{username} left: " <> Kernel.inspect(msg))
 
     if Monitor.get_scores(room_id) == [] do
-        Monitor.shutdown(room_id)
-        QuestionChronicle.remove_room_state(room_id)
+      Monitor.shutdown(room_id)
+      QuestionChronicle.remove_room_state(room_id)
     end
   end
 
@@ -62,11 +65,11 @@ defmodule Aion.RoomChannel do
     username = UserSocket.get_user_name(socket)
     user_id = UserSocket.get_user_id(socket)
     evaluation = Monitor.new_answer(room_id, answer, username, user_id)
-    send_feedback socket, evaluation
+    send_feedback(socket, evaluation)
 
     if evaluation == 1.0 do
-      send_scores(socket)
       change_question(socket)
+      send_scores(socket)
       send_current_question(socket)
       QuestionChronicle.change_room_state(room_id)
     end
@@ -109,39 +112,41 @@ defmodule Aion.RoomChannel do
     {:noreply, socket}
   end
 
-  intercept ["presence_diff"]
+  intercept(["presence_diff"])
 
   def handle_out("presence_diff", %{joins: _, leaves: _}, socket) do
-      # NOTE: This function currently sends scores.
-      # 1. We may update it later to only send the presence diff and handle it
-      # properly on the frontend side
-      # 2. The send scores here is called only when there are > 1 people in
-      # the room. If you're the first person joining the room, you're going
-      # to receive the scores from :after_join.
-      send_scores(socket)
+    # NOTE: This function currently sends scores.
+    # 1. We may update it later to only send the presence diff and handle it
+    # properly on the frontend side
+    # 2. The send scores here is called only when there are > 1 people in
+    # the room. If you're the first person joining the room, you're going
+    # to receive the scores from :after_join.
+    send_scores(socket)
 
-      {:noreply, socket}
+    {:noreply, socket}
   end
 
   defp send_scores(socket) do
     room_id = UserSocket.get_room_id(socket)
 
     scores = Monitor.get_scores(room_id)
-    broadcast! socket, @user_list_topic, %{users: scores}
+    broadcast!(socket, @user_list_topic, %{users: scores})
   end
 
   defp change_question(socket) do
     room_id = UserSocket.get_room_id(socket)
+    Monitor.bump_questions_asked(room_id)
     Monitor.new_question(room_id)
   end
 
   defp send_display_question(socket) do
-    broadcast! socket, @display_question_topic, %{}
+    broadcast!(socket, @display_question_topic, %{})
   end
 
   defp send_question_break(socket) do
-    broadcast! socket, @question_break_topic, %{}
+    broadcast!(socket, @question_break_topic, %{})
     change_question(socket)
+    send_scores(socket)
     :timer.send_after(1000, :next_question_timeout)
   end
 
@@ -151,16 +156,20 @@ defmodule Aion.RoomChannel do
     question = Monitor.get_current_question(room_id)
     image_name = if question.image_name == nil, do: "", else: question.image_name
 
-    broadcast! socket, @current_question_topic, %{content: question.content, image_name: image_name}
+    broadcast!(socket, @current_question_topic, %{
+      content: question.content,
+      image_name: image_name
+    })
   end
 
   defp send_feedback(socket, evaluation) do
-    feedback = cond do
-      evaluation == 1.0 -> :correct
-      evaluation > 0.8 -> :close
-      true -> :incorrect
-    end
+    feedback =
+      cond do
+        evaluation == 1.0 -> :correct
+        evaluation > 0.8 -> :close
+        true -> :incorrect
+      end
 
-    push socket, @answer_feedback_topic, %{"feedback" => feedback}
+    push(socket, @answer_feedback_topic, %{"feedback" => feedback})
   end
 end
