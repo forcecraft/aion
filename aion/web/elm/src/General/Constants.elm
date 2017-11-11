00d579c693c3d19044581ea216c46cb76f385a31
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-11-11 14:43:06 +0100

    fix: Make footer shorter

--- aion/web/elm/src/General/Constants.elm
+++ aion/web/elm/src/General/Constants.elm
@@ -30,8 +30,5 @@ footerContent : List String
 footerContent =
     [ "JPKS jest projektem niekomercyjnym, stworzonym ku nauce oraz uciesze uczniów i wszystkich osób lubiących tego typu rozrywki."
     , "Autorem oryginalnego pomysłu (PKS = Projekt-Klient-Serwer, napisanego w ANSI C) jest Marek Lipert, zaś cały projekt był zgłoszony w programie Diversity firmy Motorola."
-    , "W 2007 r. kod systemu został napisany od zera w języku Java przez AD, aby umożliwić zamieszczanie obrazków."
-    , "Zdjęcia i obrazki towarzyszące pytaniom zostały zaczerpnięte z bardzo różnych źródeł."
-    , "Treść zdecydowanej większości pytań jest autorstwa AD − trochę pytań podrzucili uczniowie, trochę jest zasłyszanych z rozmaitych quizów w TV."
     , "Jeśli właściciel praw autorskich do jakiegoś materiału nie wyraża zgody na jego wykorzystanie, prosimy o kontakt z autorem quizu (Andrzej Dyrek na fb)."
     ]
