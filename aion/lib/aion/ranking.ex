817245cc23c450038cd4cb68a2e0c6f4f7e3c68d
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2017-11-15 23:28:47 +0100

    Change API ranking data format. Now we aggregate user scores by category.

--- aion/lib/aion/ranking.ex
+++ aion/lib/aion/ranking.ex
@@ -1,23 +1,34 @@
 defmodule Aion.Ranking do
   @moduledoc """
-  This module provides logic for generating rankings
+  This module provides logic for generating ranking data
   """
   import Ecto.Query, only: [from: 2]
   alias Aion.{
     Repo,
     User,
+    Category,
     UserCategoryScore
   }
 
-  @type user_general_score :: %{user_name: String.t, score: integer}
+  @type category_score_t :: %{category_id: integer, category_name: String.t, scores: (list user_score_t)}
+  @type user_score_t :: %{user_name: String.t, score: integer}
+  @type raw_data_t :: %{category_id: integer, category_name: String.t, user_name: String.t, score: integer}
 
-  @spec general :: list user_general_score
-  def general do
+  @spec data :: list category_score_t
+  def data do
+    query_data()
+      |> Enum.group_by(&%{category_id: &1.category_id, category_name: &1.category_name} , &%{user_name: &1.user_name, score: &1.score})
+      |> Map.to_list()
+      |> Enum.map(&Map.put(elem(&1, 0), :scores, elem(&1, 1)))
+  end
+
+  @spec query_data :: list raw_data_t
+  def query_data do
     Repo.all(
       from q in User,
       join: ucs in UserCategoryScore, on: q.id == ucs.user_id,
-      group_by: q.name,
-      select: %{user_name: q.name, score: sum(ucs.score)}
+      join: c in Category, on: c.id == ucs.category_id,
+      select: %{category_id: c.id, category_name: c.name, user_name: q.name, score: ucs.score}
     )
   end
 end
--- aion/web/views/ranking_view.ex
+++ aion/web/views/ranking_view.ex
@@ -2,11 +2,17 @@ defmodule Aion.RankingView do
   require Logger
   use Aion.Web, :view
 
-  def render("ranking.json", %{scores: scores}) do
-    %{data: render_many(scores, Aion.RankingView, "score.json", as: :user_score)}
+  def render("ranking.json", %{category_scores: category_scores}) do
+    %{rankingList: render_many(category_scores, Aion.RankingView, "category_score.json", as: :category_score)}
   end
 
-  def render("score.json", %{user_score: user_score}) do
+  def render("category_score.json", %{category_score: category_score}) do
+    %{categoryId: category_score.category_id,
+      categoryName: category_score.category_name,
+      scores: render_many(category_score.scores, Aion.RankingView, "user_score.json", as: :user_score)}
+  end
+
+  def render("user_score.json", %{user_score: user_score}) do
     %{userName: user_score.user_name, score: user_score.score}
   end
 end
