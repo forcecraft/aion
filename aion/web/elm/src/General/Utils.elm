16a5c3cdd6e166e199585ab5dece0ad954642250
Patryk Mrukot <patryk.mrukot@schibsted.pl>
2017-11-15 10:30:48 +0100

    feat: Style rooms list view

--- aion/web/elm/src/General/Utils.elm
+++ aion/web/elm/src/General/Utils.elm
@@ -3,16 +3,6 @@ module General.Utils exposing (..)
 import Array exposing (Array, fromList)
 
 
-roomsViewColorList : Array String
-roomsViewColorList =
-    fromList [ "#1abc9c", "#2ecc71", "#9b59b6", "#34495e", "#f1c40f", "#e74c3c", "#d35400" ]
-
-
-roomsDefaultColor : String
-roomsDefaultColor =
-    "#1abc9c"
-
-
 sliceList : Int -> List a -> List (List a)
 sliceList n list =
     case ( n, list ) of
