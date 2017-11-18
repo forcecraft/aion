1bd2d5ba44a012a58745afef88a3cec14e108584
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-11-18 12:36:13 +0100

    Bump latinizer version

--- aion/mix.exs
+++ aion/mix.exs
@@ -58,7 +58,7 @@ defmodule Aion.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:simetric, "~> 0.1.0"},
       {:exrm, "~> 1.0.8"},
-      {:latinizer, "~> 0.1.0"}
+      {:latinizer, "~> 0.2.0"}
     ]
   end
 
