cfc5c57e240828762a38278263f9dad21ec03fff
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-02 23:33:15 +0100

    bring back deploy logic

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,10 +5,9 @@ chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
 ssh -o "StrictHostKeyChecking no" $USER@$HOST <<EOF
-  echo "halko" > halko
   cd $DEPLOY_DIR
-#  git pull origin master 
-#  kiex use 1.4.5 
-#  make deploy-stop 
-#  make deploy
+  git pull origin master 
+  kiex use 1.4.5 
+  make deploy-stop 
+  make deploy
 EOF
