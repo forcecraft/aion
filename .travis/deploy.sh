38f62e0e0ac95eea87b532fc191620399ce522bd
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-02 23:00:40 +0100

    Do not check for host

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-ssh $USER@$IP <<EOF
+ssh -o "StrictHostKeyChecking no" $USER@$IP <<EOF
   echo "halko" > halko
   cd $DEPLOY_DIR
 #  git pull origin master 
