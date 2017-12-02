a2294eef7d35843e74f1a57f18668918bbe949ac
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-02 22:21:39 +0100

    Rename key files and add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+ssh apps@$IP <<EOF
+  cd $DEPLOY_DIR
+  git pull origin master 
+  kiex use 1.4.5 
+  make deploy-stop 
+  make deploy
+EOF
