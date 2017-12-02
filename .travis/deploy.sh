b5462735f55f16a7f1d84d3868fa3917d4d34616
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-02 23:03:38 +0100

    Add host to known hosts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-ssh -o "StrictHostKeyChecking no" $USER@$IP <<EOF
+ssh -o "StrictHostKeyChecking no" $USER@$HOST <<EOF
   echo "halko" > halko
   cd $DEPLOY_DIR
 #  git pull origin master 
