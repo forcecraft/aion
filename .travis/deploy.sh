a0a76c25ccb096a6717735aa75bdd56f23cd9268
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-02 22:59:19 +0100

    Fix user in ssh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-ssh apps@$IP <<EOF
+ssh $USER@$IP <<EOF
   echo "halko" > halko
   cd $DEPLOY_DIR
 #  git pull origin master 
