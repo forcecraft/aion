75e1874d743f8ba894d45cadf7ccad816298118a
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-03 10:36:10 +0100

    Cleanup

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,10 +4,10 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-ssh -o "StrictHostKeyChecking no" $USER@$HOST <<EOF
-  cd $DEPLOY_DIR
-  git pull origin master 
-  kiex use 1.4.5 
-  make deploy-stop 
-  make deploy
+ssh -o "StrictHostKeyChecking no" $USER@$HOST /bin/bash <<EOF
+  source ~/.bashrc;
+  cd $DEPLOY_DIR;
+  git pull origin master;
+  make deploy-stop;
+  make deploy;
 EOF
