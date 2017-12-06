691541bcbdd56d9fcf979db6d3a0d58d57a3e5a3
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-12-06 22:09:19 +0100

    Add recommended rooms display

--- aion/web/elm/src/General/Utils.elm
+++ aion/web/elm/src/General/Utils.elm
@@ -1,6 +1,9 @@
 module General.Utils exposing (..)
 
+import Msgs exposing (Msg)
 import Array exposing (Array, fromList)
+import Html exposing (Html, text)
+import RemoteData exposing (WebData)
 
 
 sliceList : Int -> List a -> List (List a)
@@ -14,3 +17,19 @@ sliceList n list =
 
         ( n, list ) ->
             (List.take n list) :: (sliceList n (List.drop n list))
+
+
+displayWebData : WebData a -> (a -> Html Msg) -> Html Msg
+displayWebData webData fun =
+    case webData of
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Loading ->
+            text "Loading..."
+
+        RemoteData.Success data ->
+            fun data
+
+        RemoteData.Failure error ->
+            text (toString error)
